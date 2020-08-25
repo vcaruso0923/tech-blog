bd73d79c8553ee93b153ae52723f9234f271b104
vcaruso <vinnycar0923@gmail.com>
2020-08-25 16:28:08 -0700

    Debugging heroku

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
