5b61d2abd770784ee3fde035f48b62777956db2a
vcaruso <vinnycar0923@gmail.com>
2020-08-24 19:49:42 -0700

    setup handlebars

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
